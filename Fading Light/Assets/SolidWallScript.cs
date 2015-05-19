663f2849d079989b6fbb72219ed3b82da8e6c660
RUMMAKER <LouieShenSecond@gmail.com>
2015-05-19 15:58:19 -0700

    Create mesh
    http://wiki.unity3d.com/index.php?title=Triangulator
    using this rediculously mathy thing, we can create meshes now :D

--- Fading Light/Assets/SolidWallScript.cs
+++ Fading Light/Assets/SolidWallScript.cs
@@ -8,6 +8,11 @@ public class SolidWallScript : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		col = GetComponent<PolygonCollider2D>();
+
+		// Set up game object with mesh;
+		gameObject.AddComponent(typeof(MeshRenderer));
+		MeshFilter filter = gameObject.AddComponent(typeof(MeshFilter)) as MeshFilter;
+		filter.mesh = createMesh(col.points);
 	}
 	
 	// Update is called once per frame
@@ -25,6 +30,29 @@ public class SolidWallScript : MonoBehaviour {
 	void OnBecameVisible() {
 
 		DataBase.corners.Add(col);
+	}
+
+	Mesh createMesh(Vector2[] points)
+	{
+		Triangulator t = new Triangulator (points);
+
+		// Use the triangulator to get indices for creating triangles
+		int[] indices = t.Triangulate();
+
+		// Create the Vector3 vertices
+		Vector3[] vertices = new Vector3[points.Length];
+		for (int i=0; i<vertices.Length; i++) {
+			vertices[i] = new Vector3(points[i].x, points[i].y, 0);
+		}
+
+		// Create the mesh
+		Mesh msh = new Mesh();
+		msh.vertices = vertices;
+		msh.triangles = indices;
+		msh.RecalculateNormals();
+		msh.RecalculateBounds();
+
+		return msh;
 	}
 
 }
