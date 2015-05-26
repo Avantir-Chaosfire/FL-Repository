ad6b5702b17aec7281b8babd26f127cf18059b13
Avantir Chaosfire <qulego@hotmail.com>
2015-05-25 20:02:11 -0700

    Main Menu Stuff

---
+++ Fading Light/Assets/Menu Scenes/OptionsButtons.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OptionsButtons : MonoBehaviour
+{
+	
+	void Start(){}
+	
+	void Update()
+	{
+		
+		if(Input.GetKey("escape"))
+		{
+			
+			GoMainMenu();
+			
+		}
+		
+	}
+	
+	public void GoMainMenu()
+	{
+		
+		Application.LoadLevel(0);
+		
+	}
+	
+}
---
+++ Fading Light/Assets/Menu Scenes/SingleplayerButtons.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SingleplayerButtons : MonoBehaviour
+{
+
+	void Start(){}
+	
+	void Update()
+	{
+
+		if(Input.GetKey("escape"))
+		{
+			
+			GoMainMenu();
+			
+		}
+	
+	}
+
+	public void GoMainMenu()
+	{
+
+		Application.LoadLevel(0);
+
+	}
+
+	public void StartGame()
+	{
+
+		Application.LoadLevel(5);
+
+	}
+
+}
